30e54ea243b74057f740a313ec9968b31abf8a0e
Amal Shaji Michael <183818109+amalshajimichaelk@users.noreply.github.com>
2025-08-06 10:50:43 +0530

    Add files via upload

---
+++ BookDetails.java
@@ -0,0 +1,19 @@
+package demo;
+class Book{
+	String title;
+	String author;
+	final int bookID;
+	static int bookCounter=1000;
+	final static String LIBRARY_NAME="Central Library.";
+	Book(){
+		this.bookID = 0;
+		title="title";
+		author="author";
+	}
+	Book(String title,String author){
+		
+	}
+}
+public class BookDetails {
+
+}
